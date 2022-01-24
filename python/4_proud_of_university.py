07b39edbcee3cb26ba36c05439ea9bd7dbe1af53
Oleg <82169861+mazzdriver@users.noreply.github.com>
2022-01-24 14:04:32 +0300

    4th step on go

--- python/4_proud_of_university.py
+++ python/4_proud_of_university.py
@@ -1,8 +1,3 @@
-# Функция make_report_about_top3 принимает словарь (students_avg_scores), в котором ключами являются имена студентов, а значениями — средний балл за всю учебу. Функция находит ТОП-3 студентов, чей средний балл выше, чем у других. Функция возвращает ссылку на эксель-файл, в котором упомянуты 3 студента и который потом будет передан в бухгалтерию. Сам файл необходимо создать внутри функции. Важно сохранить совместимость с Excel, чтобы Ольга Петровна смогла без проблем получить всю нужную информацию. Пример входных данных приведен ниже
-
-import pandas as pd
-from itertools import islice
-
 students_avg_scores = {
     'Max': 4.964, 
     'Eric': 4.962, 
@@ -15,16 +10,14 @@ students_avg_scores = {
     'Don': 4.936, 
     'Zoi': 4.937}
 
+import pandas as pd
+
 def make_report_about_top3(avg_scores=dict):
     file = 'top_3_students.xlsx'
-    avg_scores = dict(sorted(avg_scores.items(), key=lambda item: item[1]))
-    top_3 = pd.DataFrame(avg_scores, index = [0])
-    
-    top_3.to_excel(file)
-    # with xlsxwriter.Workbook(file) as workbook:
-    #     worksheet = workbook.add_worksheet()
-    #     for row_name, data in enumerate(top3):
-    #         worksheet.write_row(row_name, 0, data)
+    avg_scores = dict(sorted(avg_scores.items(), key=lambda item: item[1], reverse=True))
+    top_3 = pd.DataFrame.from_dict(avg_scores, orient='index',columns=None)
+    top_3 = top_3[:3]
+    top_3.to_excel(file, header=False)
     return file
 
 print(make_report_about_top3(students_avg_scores))
