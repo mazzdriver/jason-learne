094aa5bee631e9a26a45bc791bb4f01b23a39248
Oleg <82169861+mazzdriver@users.noreply.github.com>
2022-01-28 19:02:43 +0300

    Trying to model

--- python/Django/cinema/cinema/settings.py
+++ python/Django/cinema/cinema/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from python.Django.cinema.movie.models import Movie
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
